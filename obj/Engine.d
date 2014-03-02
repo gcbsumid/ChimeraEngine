9078bb438795d24b7af73580dc2c610be14e798e
Christian Sumido <gcbsumid@gmail.com>
2014-03-01 16:53:28 -0800

    Removing old stuff and object files.

--- obj/Engine.d
+++
@@ -1,9 +0,0 @@
-obj/Engine.o: src/system/Engine.cpp src/system/Engine.hpp \
-  src/system/../graphics/GraphicsSystem.hpp \
-  src/system/../input/InputSystem.hpp
-
-src/system/Engine.hpp:
-
-src/system/../graphics/GraphicsSystem.hpp:
-
-src/system/../input/InputSystem.hpp:
--- obj/main.d
+++
@@ -1,9 +0,0 @@
-obj/main.o: src/system/main.cpp src/system/Engine.hpp \
-  src/system/../graphics/GraphicsSystem.hpp \
-  src/system/../input/InputSystem.hpp
-
-src/system/Engine.hpp:
-
-src/system/../graphics/GraphicsSystem.hpp:
-
-src/system/../input/InputSystem.hpp:
